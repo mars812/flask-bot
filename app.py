bed0d7d77d9e6fa2fb87e7e0152c3302136603f3
mars812 <mars812@mail.ru>
2025-05-26 01:58:05 +0700

    app.py

--- app.py
+++ app.py
@@ -16,5 +16,11 @@ def webhook():
         json_string = request.get_data().decode('utf-8')
         update = telebot.types.Update.de_json(json_string)
         bot.process_new_updates([update])
-    return '', 200
+        return '', 200
+    else:
+        return 'Invalid request', 403
 
+# Обработка команды /start
+@bot.message_handler(commands=['start'])
+def start_handler(message):
+    bot.send_message(message.chat.id, "Привет! Бот запущен через Render и webhook ✅")
