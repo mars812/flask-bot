fabb4c4ac2eff729041b65207b3b67a7023a9fb8
mars812 <mars812@mail.ru>
2025-05-26 01:35:21 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+import telebot
+
+API_TOKEN = '7671997445:AAEyZlmmYLOeVOD8PalzgUTjdeYhGs3bEfE'
+bot = telebot.TeleBot(API_TOKEN)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Бот работает!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    if request.headers.get('content-type') == 'application/json':
+        json_string = request.get_data().decode('utf-8')
+        update = telebot.types.Update.de_json(json_string)
+        bot.process_new_updates([update])
+    return '', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
